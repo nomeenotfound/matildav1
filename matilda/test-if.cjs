036e19d467f0dfa68d6b2276d8e3f18ab56be2b6
nomeenotfound <122631520+nomeenotfound@users.noreply.github.com>
2026-08-15 20:13:06 +0530

    Delete matilda directory

--- matilda/test-if.cjs
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://cdn.iframe.ly/api/iframe?url=https://jumpshare.com/v/amgavbNZhiAwzQ2pQ9ci&api_key=428945fa641176b6', (res) => {
-  let data = '';
-  res.on('data', (c) => data += c);
-  res.on('end', () => console.log(data));
-});
